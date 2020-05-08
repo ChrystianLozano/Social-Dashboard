dbd07c270eb9d77f4729fe63fb871af24c7e32b5
Chrystian Lozano <chrystianlozano@icloud.com>
2020-05-08 17:16:53 -0500

    presentando los data por props

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './card.css'
-const Card = ({username}) => {
+const Card = ({username, followers, todayFollower, solcialIcon}) => {
 
     return (
         <article className="card facebook">
         <p className="card-title">
-          <img src="images/icon-facebook.svg" alt="" />
+          <img src={solcialIcon} alt="" />
           {username}
         </p>
         <p className="card-followers">
-          <span className="card-followers-number">1483</span>
+          <span className="card-followers-number">{followers}</span>
           <span className="card-followers-title">Followers</span>
         </p>
         <p className="card-today">
           <img src="images/icon-up.svg" alt="" />
-          12 Today
+          {todayFollower} Today
         </p>
       </article>
      );
--- src/TopCardList.js
+++ src/TopCardList.js
@@ -2,10 +2,22 @@ import React, {Fragment} from 'react';
 import Card from './Card'
 
 const cardListData = [
-  {username: '/Chrystian'},
-  {username: '@mudo_'},
-  {username: 'mudo_'},
-  {username: '/ChrystianLozano'},
+  {username: '/Chrystian',
+   followers: 1483,
+   todayFollower: 12,
+   solcialIcon: '/images/icon-facebook.svg'},
+  {username: '@mudo_',
+   followers: '28k',
+   todayFollower: 40,
+   solcialIcon: '/images/icon-twitter.svg'},
+  {username: 'mudo_',
+   followers: '6k',
+   todayFollower: 20,
+   solcialIcon: '/images/icon-instagram.svg'},
+  {username: '/ChrystianLozano',
+   followers: '12k',
+   todayFollower: 29,
+   solcialIcon: '/images/icon-youtube.svg'},
 ]
 
 const TopCardList = () => {
@@ -15,8 +27,8 @@ const TopCardList = () => {
       <div className="wrapper">
         <div className="grid">
           {
-            cardListData.map(({username}) => (
-              <Card key={username} username={username}/>
+            cardListData.map(({username, followers, todayFollower, solcialIcon}) => (
+              <Card key={username} username={username} followers={followers} todayFollower={todayFollower} solcialIcon={solcialIcon}/>
             ))
           }
 
